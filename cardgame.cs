b61fd0ba65667b967e13c4729c9cd066c82b2297
dayanjr <dayanfrazao@byui.edu>
2022-10-04 14:21:43 -0600

    up

--- cardgame.cs
+++ cardgame.cs
@@ -1,1 +1,13 @@
-yo mama
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 15;
+      Console.WriteLine(myNum);
+     }
+  }
+}
